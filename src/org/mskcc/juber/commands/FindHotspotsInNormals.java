654b288398bd3e49d9fb983c7d5950749c5332cb
Juber Patel <juberpatel@gmail.com>
2019-08-22 15:33:27 -0400

    modifications to hotspots in normals code

--- src/org/mskcc/juber/commands/FindHotspotsInNormals.java
+++ src/org/mskcc/juber/commands/FindHotspotsInNormals.java
@@ -6,12 +6,17 @@ package org.mskcc.juber.commands;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -77,16 +82,151 @@ public class FindHotspotsInNormals
 		getSampleHotspots(normalHotspots.keySet(), pileupList, tumorHotspots,
 				"Tumor");
 
-		// write output
+		processAndPrint(normalHotspots, tumorHotspots, hotspots, pileupList);
+
+		/*
+		 * // write output
+		 * BufferedWriter writer = new BufferedWriter(
+		 * new FileWriter("hotspots-in-normals.txt"));
+		 * writer.write(
+		 * "Mutation\tSample\tPatient\tSampleType\tAltCount\tTotal\tAF\n");
+		 * 
+		 * printTable(normalHotspots, hotspots, "Normal", writer);
+		 * printTable(tumorHotspots, hotspots, "Tumor", writer);
+		 * 
+		 * writer.close();
+		 */
+
+	}
+
+	private static void processAndPrint(
+			Map<String, Map<String, Integer[]>> normalHotspots,
+			Map<String, Map<String, Integer[]>> tumorHotspots,
+			Map<String, String> hotspots, File pileupList) throws IOException
+	{
+		DecimalFormat df = new DecimalFormat("#.####");
+
 		BufferedWriter writer = new BufferedWriter(
 				new FileWriter("hotspots-in-normals.txt"));
-		writer.write("Mutation\tSample\tSampleType\tAltCount\tTotal\tAF\n");
+		writer.write(
+				"Mutation\tSample\tPatient\tSampleType\tAltCount\tTotal\tAF\n");
 
-		printTable(normalHotspots, hotspots, "Normal", writer);
-		printTable(tumorHotspots, hotspots, "Tumor", writer);
+		Map<String, Set<String>> samplesPerPatient = loadPatientsamples(
+				pileupList);
+
+		for (String patient : samplesPerPatient.keySet())
+		{
+			Set<String> patientSamples = samplesPerPatient.get(patient);
+			Set<String> inPatientNormals = new HashSet<String>();
+
+			// print normal and tumor hotspots for the patient
+			for (String mutation : normalHotspots.keySet())
+			{
+				// print normal hotspots
+				Map<String, Integer[]> counts = normalHotspots.get(mutation);
+				for (String sample : counts.keySet())
+				{
+					if (patientSamples.contains(sample))
+					{
+						inPatientNormals.add(mutation);
+						String mutationName = hotspots.get(mutation);
+						Integer[] pair = counts.get(sample);
+						double af = 0;
+						if (pair[1] != 0)
+						{
+							af = (pair[0] * 1.0) / pair[1];
+						}
+
+						writer.write(mutationName + "\t" + sample + "\t"
+								+ patient + "\tNormal\t" + pair[0] + "\t"
+								+ pair[1] + "\t" + df.format(af) + "\n");
+					}
+				}
+
+				if (!inPatientNormals.contains(mutation))
+				{
+					continue;
+				}
+
+				// print tumor counts for the same mutation for the same patient
+				counts = tumorHotspots.get(mutation);
+				for (String sample : counts.keySet())
+				{
+					if (patientSamples.contains(sample))
+					{
+						String mutationName = hotspots.get(mutation);
+						Integer[] pair = counts.get(sample);
+						double af = 0;
+						if (pair[1] != 0)
+						{
+							af = (pair[0] * 1.0) / pair[1];
+						}
+
+						writer.write(mutationName + "\t" + sample + "\t"
+								+ patient + "\tMatchedTumor\t" + pair[0] + "\t"
+								+ pair[1] + "\t" + df.format(af) + "\n");
+					}
+				}
+
+			}
+
+			// print other mutations in tumors for the same patient which are
+			// not in his/her normals
+			for (String mutation : tumorHotspots.keySet())
+			{
+				Map<String, Integer[]> counts = tumorHotspots.get(mutation);
+				for (String sample : counts.keySet())
+				{
+					if (patientSamples.contains(sample))
+					{
+						String mutationName = hotspots.get(mutation);
+						Integer[] pair = counts.get(sample);
+						double af = 0;
+						if (pair[1] != 0)
+						{
+							af = (pair[0] * 1.0) / pair[1];
+						}
+
+						if (!inPatientNormals.contains(mutation)
+								&& pair[0] >= 3)
+						{
+							writer.write(mutationName + "\t" + sample + "\t"
+									+ patient + "\tTumor\t" + pair[0] + "\t"
+									+ pair[1] + "\t" + df.format(af) + "\n");
+						}
+					}
+				}
+			}
+
+		}
 
 		writer.close();
 
+	}
+
+	private static Map<String, Set<String>> loadPatientsamples(File pileupList)
+			throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(pileupList));
+		String line = reader.readLine();
+		String[] tokens = null;
+
+		Map<String, Set<String>> patientSamples = new HashMap<String, Set<String>>();
+		while ((line = reader.readLine()) != null)
+		{
+			tokens = line.split("\t");
+			Set<String> s = patientSamples.get(tokens[1]);
+			if (s == null)
+			{
+				s = new HashSet<String>();
+				patientSamples.put(tokens[1], s);
+			}
+
+			s.add(tokens[0]);
+		}
+
+		reader.close();
+		return patientSamples;
 	}
 
 	private static void printTable(
@@ -132,14 +272,14 @@ public class FindHotspotsInNormals
 			tokens = line.split("\t");
 
 			// only look at specified type
-			if (!tokens[1].equals(type))
+			if (!tokens[2].equals(type))
 			{
 				continue;
 			}
 
 			String sample = tokens[0];
 			BufferedReader pileupReader = new BufferedReader(
-					new FileReader(tokens[2]));
+					new FileReader(tokens[3]));
 			String pileupLine = null;
 			String[] pileupTokens = null;
 
@@ -167,7 +307,8 @@ public class FindHotspotsInNormals
 							+ "\t" + ref + "\t" + alleles[i];
 					int altCount = Integer.parseInt(pileupTokens[i + 4]);
 
-					if (hotspots.contains(mutation) && altCount >= 3)
+					if (hotspots.contains(mutation)
+							&& (type == "Tumor" || altCount >= 3))
 					{
 						Map<String, Integer[]> counts = hotspotCounts
 								.get(mutation);
